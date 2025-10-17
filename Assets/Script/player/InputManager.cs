e169c443be8e5b741c9a2d339c8b96955d5d2297
Martin Koubek <147321704+Martin-Koubek@users.noreply.github.com>
2025-10-17 20:16:15 +0200

    pohyb hráče a kamery

---
+++ Assets/Script/player/InputManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private static InputManager instance;
+
+    public static InputManager Instance
+    {
+        get { return instance; }
+    }
+
+    private PlayerControls playerControls;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this) { Destroy(this.gameObject); }
+        else instance = this;
+        playerControls = new PlayerControls();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
+    public Vector2 GetPlayerMovement()
+    {
+        return playerControls.Player.Movement.ReadValue<Vector2>();
+    }
+    public bool PlayerDashed()
+    {
+        return playerControls.Player.Dash.triggered;
+    }
+}
--- Assets/Script/player/PlayerController.cs
+++ Assets/Script/player/PlayerController.cs
@@ -1,31 +1,53 @@
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    private float _speed;
-    private float _sprintSpeed;
-    private bool _sprintTriggered;
-    private PlayerInputController _playerInputController;
+    private float playerSpeed = 2f;
+    [SerializeField]
+    private float dashDistance = 5f;
+    [SerializeField]
+    private float rotationSpeed = 4f;
 
-    private void Awake()
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool Dashed;
+    private InputManager inputManager;
+    [SerializeField]
+    private Transform cameraMainTransform;
+    [Header("Animator")]
+    [SerializeField]
+    private Animator animator;
+    
+
+    private void Start()
     {
-        _playerInputController = GetComponent<PlayerInputController>();
-
-        _playerInputController.OnSprintButtonPressed += SprintButtonPressed;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        controller = gameObject.GetComponent<CharacterController>();
+        inputManager = InputManager.Instance;
+        cameraMainTransform = Camera.main.transform;
     }
-    private void FixedUpdate()
-    {
-        Vector3 positionChange = new Vector3(
-            _playerInputController.MovementInputVector.x,
-            0,
-            _playerInputController.MovementInputVector.y) * Time.deltaTime * _speed;
 
-        transform.position += positionChange;
-    }
-
-    private void SprintButtonPressed()
+    private void Update()
     {
-        _sprintTriggered = true;
+        Vector2 movement = inputManager.GetPlayerMovement();
+        Vector3 move = new Vector3(movement.x, 0f, movement.y);
+        move = cameraMainTransform.forward * move.z + cameraMainTransform.right * move.x;
+        move.y = 0f;    
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if(movement != Vector2.zero)
+        {
+            float targetAngle = Mathf.Atan2(movement.x, movement.y) * Mathf.Rad2Deg + cameraMainTransform.eulerAngles.y;
+            Quaternion rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+        }
+
+        if(inputManager.PlayerDashed() && !Dashed)
+        {
+
+        }
     }
 }
