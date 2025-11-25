06df64c84335aa46b829465d3a7dfeee0296c4f1
Martin Koubek <147321704+Martin-Koubek@users.noreply.github.com>
2025-11-25 19:06:03 +0100

    pohyb nepřítele
    nepřítel zapomocí NavMeshe si najde náhodnou pozici na kterou se přesune

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using TMPro;
+using UnityEditor.Search;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UI;
@@ -22,20 +23,34 @@ public class Enemy : MonoBehaviour
 
     //navigation
     private NavMeshAgent agent;
+    public Transform player;
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    //patroling
+    public Vector3 walkingPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //attacking
+    public float timeBetweenAttacks;
+    public bool attacked;
+
+    //states
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
 
     //animator
     private Animator anim;
     private int AnimatorDeathIdle;
+    private int AnimatorMoveZId;
+    private int AnimatorAttackId;
+
 
 
     private IEnumerator DestroyAfterDelay(Collider collider)
     {
-        yield return new WaitForSeconds(0.5f);
-        collider.isTrigger = true;
-
         yield return new WaitForSeconds(5f);
         Destroy(gameObject);
-
     }
 
     private void Awake()
@@ -45,19 +60,32 @@ public class Enemy : MonoBehaviour
         _maxHealth = basicHealth * Lvl;
         curentHealth = _maxHealth;
 
+        player = GameObject.Find("Knight").transform;
         agent = GetComponent<NavMeshAgent>();
+
         spawner = GetComponentInParent<EnemyRespawn>();
+
         anim = GetComponent<Animator>();
-        coll = GetComponent<Collider>();
+        AnimatorMoveZId = Animator.StringToHash("MoveZ");
         AnimatorDeathIdle = Animator.StringToHash("Dead");
+
+        coll = GetComponent<Collider>();
+
     }
     private void Update()
     {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange) Patroling();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if(playerInSightRange && playerInAttackRange) AttackPlayer();
+
         health.text = curentHealth + "/" + _maxHealth;
 
         healthFillBar.fillAmount = curentHealth / _maxHealth;
 
-        if(curentHealth <= 0)
+        if (curentHealth <= 0)
         {
             anim.SetBool(AnimatorDeathIdle, true);
             Die();
@@ -75,6 +103,62 @@ public class Enemy : MonoBehaviour
 
     private void Move()
     {
-        
+
+    }
+
+    private void Patroling()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+
+        if(walkPointSet) agent.SetDestination(walkingPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkingPoint;
+
+        if(distanceToWalkPoint.magnitude < 1f) walkPointSet=false;
+    }
+    private void SearchWalkPoint()
+    {
+
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkingPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+        //walkPointSet = true;
+        if (Physics.Raycast(walkingPoint, -transform.up, 5f, whatIsGround))
+        {
+            anim.SetFloat(AnimatorMoveZId, 0.5f);
+            walkPointSet = true;
+        }
+    }
+    private void ChasePlayer()
+    {
+        if(Physics.Raycast(player.transform.position, -transform.up, 5f, whatIsGround))
+        {
+            anim.SetFloat(AnimatorMoveZId, 1f);
+            agent.SetDestination(player.position);
+        }
+        else Patroling();
+       
+    }
+    private void AttackPlayer()
+    {
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        if (!attacked)
+        {
+            //attack code
+
+
+
+            attacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    private void ResetAttack()
+    {
+        attacked=false;
     }
 }
