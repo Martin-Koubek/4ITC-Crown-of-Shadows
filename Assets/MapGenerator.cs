21ab840a60a438facb096ce7883c1b7efec8640e
Martin Koubek <147321704+Martin-Koubek@users.noreply.github.com>
2025-12-06 12:33:57 +0100

    vytvoření koncové místnosti

--- Assets/MapGenerator.cs
+++ Assets/MapGenerator.cs
@@ -1,16 +1,59 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public startRoom startRoom;
+    public EndRoom endRoom;
 
-    // Update is called once per frame
-    void Update()
+    private Room newRoom;
+    private Room lastRoom;
+
+    private Transform _connectionPointStart;
+    private Transform _connectionPointEnd;
+
+    [SerializeField] private List<Room> rooms = new List<Room>();
+    [SerializeField] private GameObject _FloorTile;
+    [SerializeField] private GameObject _Wall;
+    System.Random rnd = new();
+
+    private void Awake()
     {
-        
+    }
+    public void NewFloor()
+    {
+        int roomCount = rnd.Next(5, 11);
+        for (int i = 0; i < roomCount + 1; i++)
+        {
+            if(i == 0)
+            {
+                Vector3 roomPos = new Vector3(rnd.Next(50, 101), 0, rnd.Next(50, 101));
+                _connectionPointStart = startRoom.connectionPoint;
+                newRoom = Instantiate(rooms[rnd.Next(0, rooms.Count)], roomPos, Quaternion.identity);
+                _connectionPointEnd = newRoom.connectionPoint;
+                //Hall Gen script
+                lastRoom = newRoom;
+
+            }
+            else if(i == roomCount + 1)
+            {
+                _connectionPointStart = lastRoom.connectionPoint;
+                _connectionPointEnd = endRoom.connectionPoint;
+                //Hall Gen script
+            }
+            else
+            {
+                Vector3 roomPos = new Vector3(rnd.Next(50, 101), 0, rnd.Next(50, 101));
+                newRoom = Instantiate(rooms[rnd.Next(0, rooms.Count)], roomPos, Quaternion.identity);
+                _connectionPointStart = lastRoom.connectionPoint;
+                _connectionPointEnd = newRoom.connectionPoint;
+                //Hall Gen script
+
+                lastRoom = newRoom;
+            }
+        }
     }
 }
---
+++ Assets/startRoom.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class startRoom : MonoBehaviour
+{
+    public Transform connectionPoint;
+
+}
